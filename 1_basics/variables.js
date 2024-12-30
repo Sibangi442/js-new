a862f1303743c412ac4b02596dd0c96082e211f1
Sibangi442 <sibangipadhi93@gmail.com>
2024-12-30 16:37:32 +0000

    const,let and var keywords

---
+++ 1_basics/variables.js
@@ -0,0 +1,12 @@
+const accountId="14456"
+let accountEmail ="kalyanipadhi9520@gmail.com"
+var accountPassword ="1234"
+accountCity ="jaipur"
+let accountState;
+
+accountEmail="abc2gmail.com"
+accountPassword="4444"
+accountCity ="Bhawanipatna"
+
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword,accountCity,accountState])
