ea4d3722536002f3e08f0e268a77412ed78565f2
Sibangi442 <sibangipadhi93@gmail.com>
2024-12-31 14:33:46 +0000

    conversion of datatypes

---
+++ 1_basics/4_conversion.js
@@ -0,0 +1,30 @@
+//To convert something to Number
+//let score = "33"
+//let score ="33abc"
+//let score ="null" -output will be 0(zero)
+//let score = undefined -output will be NaN
+//let score = true -output will be 1
+//let score = false - output will be 0
+//let score = "sibangi"- output will be NaN
+
+
+//console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//To convert to boolean
+// let isloggedIn = 1
+// let booleanIsLoggedIn = Boolean(isloggedIn)
+// console.log(booleanIsLoggedIn);
+// 1 -  true; 0 - false
+//"" - false(for empty string)
+//"sibangi"- true
+
+//To convert somrthing to string
+let number = 45
+let numbertostring = String(number);
+console.log(numbertostring)
