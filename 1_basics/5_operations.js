4c6d3391988bd44f11dea25901f11d382ac60b18
Sibangi442 <sibangipadhi93@gmail.com>
2024-12-31 16:29:45 +0000

    different operations

---
+++ 1_basics/5_operations.js
@@ -0,0 +1,28 @@
+let value = 3
+let negvalue = -value
+//console.log(negvalue)
+
+// console.log(2+3)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+
+ let str1 = "hello"
+ let str2 =" sibangi"
+ let str3 = str1 +str2
+//  console.log(str3);
+
+//  console.log("1" +2);
+//  console.log(1 +"2");
+    console.log("1" +2 +2);
+    console.log(1 +2 +"2");
+// console.log(true); // output will be true
+//  console.log(+true); // here output will be 1
+//  console.log(+""); // output will be 0
+
+ let num1 ,num2, num3
+ num1 = num2 = num3 = 2+2 // should not be written like this
+
+ let gamecounter = 100
+ gamecounter++;
+ //console.log(gamecounter);
