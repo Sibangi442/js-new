c31db528aee5259ddbe75b01e09fa0cee26eff43
Sibangi442 <sibangipadhi93@gmail.com>
2024-12-31 13:45:30 +0000

    datatypes

---
+++ 1_basics/3_datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; // treat all js code as new version
+
+//alert(3 + 3) we are using node js not browser(javascript)
+
+console.log(3 + 3);
+
+//datat types
+let name = "sibangi"
+let age = 20
+let isloggedin = true
+let state = null
+
+
+console.log(typeof "sibangi")
